89193ca6d28eb4950974e56cf10fa63f5c0eb6a0
Piwoksn <piwoksn@gmail.com>
2024-09-19 11:04:29 +0100

    made email readonly for security sake

--- myapp/views.py
+++ myapp/views.py
@@ -1563,6 +1563,7 @@ def profile(request):
                     instagramm = request.POST['instagram']
                     instagram = f"https://www.instagram.com/{instagramm}"
 
+                    record(businessname, moredesc, firstname, lastname, user, gender, phone, country, state, address, meansofid, idnumber, whatsapp, facebook, instagram)
                     user.first_name = firstname
                     user.last_name = lastname
                     user.moredesc = moredesc
@@ -1576,6 +1577,7 @@ def profile(request):
                     user.facebook = facebook
                     user.whatsapp = whatsapp
                     user.instagram = instagram
+                    user.record = f'{user}_record'+'.txt'
 
                     user.save()
                     messages.success(
