0a0e90405598da5a470678ce05190d07c20dc6f0
Piwoksn <piwoksn@gmail.com>
2025-06-14 13:36:18 +0100

    created user api, product api

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import UserView
+from .views import UserView, UserDetailView, ProductListView, ProductDetailView
 
 urlpatterns = [
-    path('', UserView.as_view(),)
+    path('', UserView.as_view()),
+    path('<int:pk>/', UserDetailView.as_view()),
+    path('products/', ProductListView.as_view()),
+    path('products/<uuid:pk>/', ProductDetailView.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -1,8 +1,22 @@
 from rest_framework import generics
 from .serializers import UserSerializer, ProductSerializer, CartItemSerializer
 from django.contrib.auth import get_user_model
+from shopapp.models import Product, CartItem
 
 # Create your views here.
 class UserView(generics.ListAPIView):
     serializer_class = UserSerializer
     queryset = get_user_model().objects.all()
+
+class UserDetailView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    queryset = get_user_model().objects.all()
+
+
+class ProductListView(generics.ListCreateAPIView):
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
+
+class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
