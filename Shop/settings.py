f234e460e9b7de47ff63e1724ba7db3cd6ffe188
Piwoksn <piwoksn@gmail.com>
2025-06-14 14:30:17 +0100

    downloaded dj-rest-auth

--- Shop/settings.py
+++ Shop/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'crispy_bootstrap5',
     'rest_framework',
-    'django_rest_auth',
+    'dj_rest_auth',
     
     # local apps
     'subscription',
--- Shop/urls.py
+++ Shop/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('', include('chats.urls')),
     path('api/', include('api.urls')),
     # 3rd Party 
+    path('api/auth/', include('dj_rest_auth.urls')),
     path('accounts/', include('allauth.urls')),
     
 ]
